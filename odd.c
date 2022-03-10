e35347dc3482f326a5eb45193842024209a7b43b
rehankhan07860 <90464921+rehankhan07860@users.noreply.github.com>
2022-03-11 00:26:44 +0530

    Add files via upload
    find the even or odd

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the no: ");
+    scanf("%d", &a);
+    if (a%2==0)
+    {
+        printf("Even");
+    }
+    else{
+        printf("odd");
+    }
+    
+}

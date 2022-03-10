a9673f8d5ccf7dc4475256f09142d106b0cc5cda
rehankhan07860 <90464921+rehankhan07860@users.noreply.github.com>
2022-03-11 00:24:34 +0530

    Add files via upload
    find the leap year

---
+++ leap.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int year;
+    printf("Enter the any year:");
+    scanf("%d", &year);
+    if (year % 400 == 0)
+    {
+      printf("It is a leap year %d", year);
+    }
+    else if (year % 100 == 0)
+    {
+        printf("It is not a leap year %d", year);
+    }
+    else if (year % 4 == 0)
+    {
+        printf("It is a leap year %d", year);
+    }else{
+        printf("It is not a leap year %d", year);
+    }
+    
+    
+    
+}

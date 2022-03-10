c9a199ac6fbcac18a3c7d73d983a4461fdbc4084
rehankhan07860 <90464921+rehankhan07860@users.noreply.github.com>
2022-03-11 00:33:20 +0530

    Add files via upload
    Find the array

---
+++ arry.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i = 0;
+    int marks[5];
+    marks[0] = 35;
+    marks[1] = 36;
+    marks[2] = 63;
+    marks[3] = 33;
+    marks[4] = 634;
+    for (int i = 0; i < 5; i++)
+    {
+       printf("\n%d", marks[i]);
+    }
+    
+}
